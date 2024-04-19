bc61f2d1d8d4b340b62b05104650a62dcb963ec1
Yosh <antonyescor4@gmail.com>
2024-04-19 15:24:13 -0600

    complethe the first part

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import MessageContainer from '../../components/messages/MessageContainer'
 
 export default function Home() {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-25'>
       <Sidebar/>
       <MessageContainer/>
     </div>
